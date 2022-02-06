a90909ca81a8e7968176f82ebf1c09e7431f360f
JD3006 <48618081+JD3006@users.noreply.github.com>
2022-02-06 11:00:09 +0800

    Update and rename CosmeticFunctions.txt to RevivePeople.cs

---
+++ Decompiled Stuff that you can use to make cool mods/good scripts/host only/RevivePeople.cs
@@ -0,0 +1,3 @@
+// GameManager Script
+MonoBehaviourPublicDi2UIObacspDi2UIObUnique.SpawnPlayer(INSERT Player id here, new Vector3(location here));
+MonoBehaviourPublicDi2UIObacspDi2UIObUnique.RespawnPlayer(INSERT Player id here, new Vector3(location here));
